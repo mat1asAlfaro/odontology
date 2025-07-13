049aea9d9e0fb02a5ac9a49aff2c4683f09b16e3
Matias Alfaro Iglesias <matialfaro757575@gmail.com>
2025-07-13 12:49:47 -0300

    Update userRoutes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const apiKeyAuth = require("../middleware/apiKeyAuth");
 const userController = require("../controllers/user.controller");
+
+router.use(apiKeyAuth);
 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
-router.post("/admins", userController.createAdmin);
-router.post("/dentists", userController.createDentist);
-router.post("/patients", userController.createPatient);
+router.post("/", userController.createUser);
+router.put("/:id", userController.updateUser);
+router.patch("/:id", userController.updateUserStatus);
 
 module.exports = router;
