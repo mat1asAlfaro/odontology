f9db7c6447723171e4b77e2775d992a327d3a00e
Matias Alfaro Iglesias <matialfaro757575@gmail.com>
2025-07-13 12:48:55 -0300

    Update userController

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,12 +1,19 @@
 const pool = require("../config/db");
 const bcrypt = require("bcrypt");
+const { logger, logWithFile } = require("../services/logger");
+const log = logWithFile(logger, __filename);
+
+const UserModel = require("../models/user.model");
+const AdminModel = require("../models/admin.model");
+const PatientModel = require("../models/patient.model");
+const DentistModel = require("../models/dentist.model");
 
 exports.getAllUsers = async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM users");
-    res.json(result.rows);
+    const result = await UserModel.getAllUsers();
+    res.json(result);
   } catch (error) {
-    console.error("Error getting users: ", error);
+    log.error("Error getting users: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
@@ -14,143 +21,134 @@ exports.getAllUsers = async (req, res) => {
 exports.getUserById = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query(`SELECT * FROM users WHERE id = ?`, [id]);
+    const result = await UserModel.getUserById(id);
 
     if (result.length === 0)
       return res.status(404).json({ error: "User not found" });
 
     res.json(result[0]);
   } catch (error) {
-    console.error("Error getting user by id: ", error);
+    log.error("Error getting user by id: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-exports.createAdmin = async (req, res) => {
-  const { firstName, lastName, email, password, role, phone } = req.body;
+exports.createUser = async (req, res) => {
+  const { email, password, role, ...userData } = req.body;
 
-  const connection = await pool.getConnection();
+  const userExists = await UserModel.getUserByEmail(email);
+  if (userExists.length !== 0) {
+    return res.status(409).json({
+      message: "User exists",
+    });
+  }
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
+    let userResult;
 
-    await connection.beginTransaction();
+    const finalUserData = { ...userData, email, password: hashedPassword };
 
-    const [userResult] = await pool.query(
-      "INSERT INTO users (first_name, last_name, email, password, role, phone) VALUES (?, ?, ?, ?, ?, ?)",
-      [firstName, lastName, email, hashedPassword, "ADMIN", phone]
-    );
-
-    const userId = userResult.insertId;
+    switch (role) {
+      case "admin":
+        userResult = await AdminModel.createAdmin(finalUserData);
+        break;
+      case "patient":
+        userResult = await PatientModel.createPatient(finalUserData);
+        break;
+      case "dentist":
+        userResult = await DentistModel.createDentist(finalUserData);
+        break;
+      default:
+        return res.status(400).json({ message: "Invalid user role" });
+    }
 
     res.status(201).json({
-      id: userId,
-      firstName,
-      lastName,
-      email,
-      hashedPassword,
-      role,
-      phone,
+      message: `User ${role} created successfully`,
     });
   } catch (error) {
-    console.error("Error creating admin: ", error);
-    res.status(500).json({ error: "Admin could not be created" });
+    log.error("Error creating user:", error);
+    res.status(500).json({ error: "User could not be created" });
   }
 };
 
-exports.createDentist = async (req, res) => {
-  const { firstName, lastName, email, password, phone, specialty } = req.body;
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { email, role, ...userData } = req.body;
 
-  const connection = await pool.getConnection();
+  const userIdExists = await UserModel.getUserById(id);
+  if (userIdExists.length === 0) {
+    return res.status(400).json({
+      message: "Non-existent user id",
+    });
+  }
+
+  const userEmailExists = await UserModel.getUserByEmail(email);
+  if (userEmailExists.length === 0) {
+    log.info("EMAIL: ", email);
+    return res.status(400).json({
+      message: "Non-existent user email",
+    });
+  }
 
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    let result;
 
-    await connection.beginTransaction();
+    const finalUserData = { ...userData, email };
 
-    const [userResult] = await connection.query(
-      "INSERT INTO users (first_name, last_name, email, password, role, phone) VALUES (?, ?, ?, ?, ?, ?)",
-      [firstName, lastName, email, hashedPassword, "DENTIST", phone]
-    );
+    switch (role) {
+      case "admin":
+        result = await AdminModel.updateAdmin(id, finalUserData);
+        break;
+      case "patient":
+        result = await PatientModel.updatePatient(id, finalUserData);
+        break;
+      case "dentist":
+        result = await DentistModel.updateDentist(id, finalUserData);
+        break;
+      default:
+        return res.status(400).json({ message: "Invalid user role" });
+    }
 
-    const userId = userResult.insertId;
-
-    await connection.query(
-      "INSERT INTO dentists (user_id, specialty) VALUES (?, ?)",
-      [userId, specialty]
-    );
-
-    await connection.commit();
-
-    res.status(201).json({
-      id: userId,
-      firstName,
-      lastName,
-      email,
-      hashedPassword,
-      role: "DENTIST",
-      phone,
-      specialty,
-    });
+    res.json({ message: `User ${role} updated successfully`, result });
   } catch (error) {
-    await connection.rollback();
-    console.error("Error creating dentist:", error);
-    res.status(500).json({ error: "Dentist could not be created" });
-  } finally {
-    connection.release();
+    console.error(error);
+    res.status(500).json({
+      message: "Error updating user",
+      error: error.message,
+    });
   }
 };
 
-exports.createPatient = async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    email,
-    password,
-    phone,
-    ci,
-    birthdate,
-    address,
-  } = req.body;
-
-  const connection = await pool.getConnection();
+exports.updateUserStatus = async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
 
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const result = await UserModel.updateUserStatus(id, status);
 
-    await connection.beginTransaction();
-
-    const [userResult] = await connection.query(
-      "INSERT INTO users (first_name, last_name, email, password, role, phone) VALUES (?, ?, ?, ?, ?, ?)",
-      [firstName, lastName, email, hashedPassword, "PATIENT", phone]
-    );
-
-    const userId = userResult.insertId;
-
-    await connection.query(
-      "INSERT INTO patients (user_id, ci, birthdate, address) VALUES (?, ?, ?, ?)",
-      [userId, ci, birthdate, address]
-    );
-
-    await connection.commit();
-
-    res.status(201).json({
-      id: userId,
-      firstName,
-      lastName,
-      email,
-      hashedPassword,
-      role: "PATIENT",
-      phone,
-      ci,
-      birthdate,
-      address,
-    });
+    res.json({ message: `User role updated successfully`, result });
   } catch (error) {
-    await connection.rollback();
-    console.error("Error creating patient:", error);
-    res.status(500).json({ error: "Patient could not be created" });
-  } finally {
-    connection.release();
+    log.error(error);
+    res.status(500).json({
+      message: "Error updating user role",
+      error: error.message,
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await UserModel.updateUserStatus(id, 0);
+
+    res.json({ message: `User deleted successfully`, result });
+  } catch (error) {
+    log.error(error);
+    res.status(500).json({
+      message: "Error updating user role",
+      error: error.message,
+    });
   }
 };
